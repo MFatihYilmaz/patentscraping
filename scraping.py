a8eb872bd3ebdc1003cb78d45d4966803cd07416
MFatihYilmaz <mfth78@hotmail.com>
2024-05-29 00:28:42 +0300

    updated patent class

--- scraping.py
+++ scraping.py
@@ -14,17 +14,25 @@ import time
 import glob
 from urllib.parse import quote
 
-from dataclasses import dataclass
-
 baslangic_zamani = time.time()
 
-patents = []
+patents = {}
 pdf_paths = {}
+
 class Patent:
-    def __init__(self, title, pdf_link, publish_date):
+    def __init__(self, index,title, pdf_link, publish_date):
+        self.index=index
         self.title = title
         self.pdf_link = pdf_link
         self.publish_date = publish_date
+
+    def to_dict(self):
+        return {
+            'index':self.index,
+            'title': self.title,
+            'pdf_link': self.pdf_link,
+            'publish_date': self.publish_date
+        }
 
 async def download_pdf(session, url, filename):
     try:
@@ -71,7 +79,7 @@ async def visit_and_fetch(url):
             if numofpatent is None:
                 print('girdi')
                 return "String is empty"
-            num = min(100, num)
+            num = 30#min(100, num)
             tasks = []
             pdfs_per_page = 20
             click_length = math.ceil(num / pdfs_per_page) - 1
@@ -80,16 +88,17 @@ async def visit_and_fetch(url):
                 print("girdi", page_number)
 
                 for i in range(pdfs_per_page):
-                    print("--------", (pdfs_per_page * page_number) + i)
+                    print("--------", (pdfs_per_page * page_number))
                     pdf_id_element = await page.query_selector(f'#applicationNumber-{(pdfs_per_page * page_number) + i}')
                     pdf_id = await pdf_id_element.text_content()
                     title_element = await page.query_selector(f'th#enhanced-table-{(pdfs_per_page * page_number) + i}')
                     title = await title_element.text_content()
                     date_element = await page.query_selector(f'td#applicationDate-{(pdfs_per_page * page_number) + i}')
                     date = await date_element.text_content()
-                    patent = Patent(title, pdf_id, date)
-                    patents.append(patent)
+                    
                     download_url = f'https://portal.turkpatent.gov.tr/anonim/arastirma/patent/sonuc/dosya?patentAppNo={pdf_id}&documentsTpye=all'
+                    patent = Patent(((pdfs_per_page * page_number) + i),title, download_url, date)
+                    patents[download_url]=patent
                     task = asyncio.create_task(download_pdf(session, download_url, f"pdf{(pdfs_per_page * page_number) + i}.pdf"))
                     tasks.append(task)
 
@@ -116,45 +125,43 @@ def delete_files(pattern):
         except OSError as e:
             print(f"{file} silinirken hata oluştu: {e}")
 
-def mainfunc(sum='',title='',start_date='',end_date=''):
-
+def mainfunc(sum='', title='', start_date='', end_date=''):
     delete_files("*.pdf")
     url = f'https://www.turkpatent.gov.tr/arastirma-yap?form=patent&params=%257b%2522abstracttr%2522%253a%2522{quote(sum)}%2522%252c%2522title%2522%253a%2522{quote(title)}%2522%252c%2522bulletinDate%2522%253a%2522{quote(start_date)}%2522%252c%2522bulletinDateLast%2522%253a%2522{quote(end_date)}%2522%257d&run=true'
     asyncio.run(main(url))
-    output,summ = pdf_analyze(pdf_paths)
+    output, summ = pdf_analyze(pdf_paths)
     output = output.split("\n")
     output.pop(0)
 
     num_threads = len(output)
     json_list = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        # İstekleri gönder
         future_to_prompt = {executor.submit(service, prompt): prompt for prompt in output}
-        # Sonuçları al
         for future in concurrent.futures.as_completed(future_to_prompt):
             prompt = future_to_prompt[future]
             try:
                 response = future.result()
                 json_text = json.loads(response)
-                sum_index=output.index(prompt)
-                json_text["Summary"]=summ[sum_index]
-                print(json_text)
-
+                sum_index = output.index(prompt)
+                json_text["Summary"] = summ[sum_index]
+                patent_index=json_text.get('Link')
+                patentval=patents.get(patent_index)
+                json_text['patent_data']=patentval.to_dict()
+                
                 json_list.append(json_text)
                 print(f"Response for '{prompt}': {response}")
             except Exception as exc:
                 print(f"An error occurred for '{prompt.splitlines()[0]}': {exc}")
-        json_data = json.dumps(json_list,ensure_ascii=False,indent=4)
-
-        with open("combined.json","w+",encoding="utf-8") as f:
+        json_data = json.dumps(json_list, ensure_ascii=False, indent=4)
+        print(json_data)
+        with open("combined.json", "w+", encoding="utf-8") as f:
             f.write(json_data)
 
-    #service(output)
     bitis_zamani = time.time()
     gecen_sure = bitis_zamani - baslangic_zamani
     print(gecen_sure)
 
     return json_data
 
-if __name__=="__main__":
-    mainfunc(title = 'rejenerasyon')
+if __name__ == "__main__":
+    mainfunc(title='rejenerasyon')
