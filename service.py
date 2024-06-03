1527b74cbf5b8ac37d26e41af842690f27241ab2
DogusGok <92594904+DogusGok@users.noreply.github.com>
2024-06-03 10:38:58 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-OPENAI_API_KEY = "sk-proj-ZRvfvlDVbrNyTAvThS59T3BlbkFJ5kTOCZzvRrJwYyGVKE9r"
+OPENAI_API_KEY = "your_API_key_here"
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 def service(text):
